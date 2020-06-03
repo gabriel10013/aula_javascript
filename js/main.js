05c6dc6e94f6014d4a586d12bce070eb0bae2879
gabrielliraaraujo <gabriel10013@hotmail.com>
2020-06-03 09:21:19 -0300

    ADICIONANDO HTML E CSS

---
+++ js/main.js
@@ -0,0 +1,48 @@
+
+var count = 0;
+while(count < 5){
+    console.log(count);
+    count = count + 1;
+};
+
+
+
+/*
+var idade = prompt("Qual sua idade? ")
+//var idade = 18;
+if(idade >= 18){
+    alert("maior de idade");
+}else{
+    alert("menor de idade");
+};
+*/
+
+//var fruta = [{nome:"maca", cor:"vermelha"}, {nome: "uva", cor:"roxa"}];
+//console.log(fruta);
+//alert(fruta[1].nome);
+
+/*
+var fruta = {nome:"maca", cor:"vermelha"};
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+//var lista = ["macao","pera","laranja"];
+//lista.push("uva");
+//lista.pop();
+
+//console.log(lista);
+//console.log(lista);
+//console.log(lista.toString())
+//console.log(lista.join(" | "))
+
+//var nome = "GAABRIEL";
+//var n1 = 5;
+//var n2 = 3;
+//var frase = "Japao melhor selecao do mundo";
+//alert(nome + " tem " + idade + " anos");
+//alert(idade+idade2);
+//console.log(nome);
+//console.log(n1*n2);
+//console.log(frase.toLowerCase());
+//alert(frase.replace("Japao", "Brasil"));
